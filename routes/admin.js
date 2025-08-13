4129be87c1feadf140da98a31171d48743450e9e
Hussam <o.hussam@gmail.com>
2025-08-13 22:20:16 +0300

    Initial commitment

---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/add-product', (req, res, next) => {
+    res.send("<form method='POST' action='/product'><input type='text' name='title' /><button type='submit'>Send</button></form>")
+});
+
+router.post('/product', (req, res, next) => {
+    res.redirect('/')
+    console.log(req.body)
+});
+
+
+module.exports = router
